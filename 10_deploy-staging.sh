2a9aed7ef863c4f21fe1bb775d5934b1d2c0951e
Rafael Benevides <rafabene@gmail.com>
2016-09-08 23:02:46 -0400

    Update project name

--- 10_deploy-staging.sh
+++ 10_deploy-staging.sh
@@ -15,11 +15,11 @@
 # limitations under the License.
 echo "Deploying to the staging area"
 oc new-project 12factor-staging
-oc policy add-role-to-user system:image-puller system:serviceaccount:12factor-staging:default -n 12fatorsdemo
-oc tag 12fatorsdemo/my12factorapp:latest 12factor-staging/my12factorapp:latest
+oc policy add-role-to-user system:image-puller system:serviceaccount:12factor-staging:default -n 12factor-dev
+oc tag 12factor-dev/my12factorapp:latest 12factor-staging/my12factorapp:latest
 oc new-app my12factorapp
 oc expose svc/my12factorapp --hostname 12factor-staging.10.1.2.2.nip.io
 oc set probe dc/my12factorapp --readiness --get-url=http://:8080/api/health
 open http://12factor-staging.10.1.2.2.nip.io/api/hello/Rafael
-oc project 12fatorsdemo
+oc project 12factor-dev
 
